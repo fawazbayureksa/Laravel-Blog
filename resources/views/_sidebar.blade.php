5ea43cba9abe0201d80804263dcad4242f892901
fawazbayureksa <fawwazbayureksa@gmail.com>
2022-01-02 14:27:09 +0800

    update view

--- resources/views/_sidebar.blade.php
+++ resources/views/_sidebar.blade.php
@@ -1,17 +1,14 @@
 
 <div class="card border-0 rounded shadow-lg">
-				<div class="card-body">
-					<div class="h3">
-						Main Menu
-					</div>
-					<div class="list-group">
-					    <a href="#" class="list-group-item list-group-item-action">DASHBOARD</a>
-					    <a href="{{route('blog.index')}}" class="list-group-item list-group-item-action">ARTIKEL</a>
-					    <a href="#" class="list-group-item list-group-item-action">PROFIL</a>
-					    <a href="#" class="list-group-item list-group-item-action">KELUAR</a>
-					</div>
-				</div>
-				<div class="card-body">
-					
-				</div>
-			</div>
+	<div class="card-body">
+		<div class="h3">
+			Main Menu
+		</div>
+		<div class="list-group">
+		    <a href="#" class="list-group-item list-group-item-action">DASHBOARD</a>
+		    <a href="{{route('blog.index')}}" class="list-group-item list-group-item-action">ARTIKEL</a>
+		    <a href="#" class="list-group-item list-group-item-action">PROFIL</a>
+		    <a href="#" class="list-group-item list-group-item-action">KELUAR</a>
+		</div>
+	</div>
+</div>
