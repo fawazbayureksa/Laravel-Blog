bb15c97440a006d589a8bba3442af5335fc9c2c1
fawazbayureksa <fawwazbayureksa@gmail.com>
2022-01-05 00:02:57 +0800

    update halaman utama

--- resources/views/components/_jumbotron.blade.php
+++ resources/views/components/_jumbotron.blade.php
@@ -1,7 +1,6 @@
-<div class="pt-4 bg-light rounded-2">
+<div class="py-5 bg-light rounded-2 mb-3">
   <div class="container py-5">
-    <h1 class="display-5 fw-bold">Custom jumbotron</h1>
-    <p class="col-md-8 fs-4">Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.</p>
-    <button class="btn btn-primary btn-lg" type="button">Example button</button>
-  </div>
+    <h1 class="display-6 fw-bold">REXXCODE</h1>
+    <p class="col-md-8 fs-5">Merupakan blog pribadi yang dikembangkan untuk media pembelajaran</p>
+   </div>
 </div>
