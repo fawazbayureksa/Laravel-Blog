322dda638cc2c6f0ef6e6b70cbf700ccdb116b37
fawazbayureksa <fawwazbayureksa@gmail.com>
2022-01-05 17:24:29 +0800

    Update halaman utama

--- resources/views/components/_jumbotron.blade.php
+++ resources/views/components/_jumbotron.blade.php
@@ -1,6 +1,8 @@
-<div class="py-5 bg-light rounded-2 mb-3">
+<div class="py-5 rounded-2 mb-3" style="background-color: #F0E68C">
   <div class="container py-5">
     <h1 class="display-6 fw-bold">REXXCODE</h1>
-    <p class="col-md-8 fs-5">Merupakan blog pribadi yang dikembangkan untuk media pembelajaran</p>
+    <p class="col-md-8 fs-5">Merupakan blog yang dikembangkan untuk media pembelajaran dan tempat berbagi ilmu</p>
+    <a href="#" class="btn btn-success btn-md rounded-lg">About</a>
+    <a href="#" class="btn btn-outline-dark btn-md rounded-lg">Daftar</a>
    </div>
 </div>
