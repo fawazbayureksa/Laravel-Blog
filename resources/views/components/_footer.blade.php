32f4b689b2156dea6ce560f0d537cf619af65f3a
fawazbayureksa <fawwazbayureksa@gmail.com>
2022-01-06 13:08:02 +0800

    update icon halaman utama

--- resources/views/components/_footer.blade.php
+++ resources/views/components/_footer.blade.php
@@ -8,18 +8,26 @@
 	.bold{
 		font-weight: bold;
 	}
+	 i.fab {
+      font-size: 2em;
+    }
 </style>
 <div class="footer bg-warning">
 	<div class="container">
 		
 	<div class="row">
 		<div class="col-md-5">
-			<h4>REXXCODE</h4>
+			<h2>REXXCODE</h2>
 			<p>Share Anything You Know</p>
+			<i class="fab fa-facebook"></i>
+			<i class="fab fa-instagram"></i>
+			<i class="fab fa-linkedin"></i>
+			<i class="fab fa-twitter"></i>
+			<i class="fab fa-github"></i>
 		</div>
 	</div>
 
-	<div class="text-center	">
+	<div class="text-center mt-3">
 			<p class="bold">2022 - Fawwaz Bayureksa</p>
 	</div>
 
