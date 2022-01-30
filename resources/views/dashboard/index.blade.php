dd8f4c6801c1ee6e6a986144d11fc918a96c04f1
fawazbayureksa <fawwazbayureksa@gmail.com>
2022-01-30 12:18:28 +0800

    update UI Landing Page

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -6,45 +6,6 @@
 
 
 @section('content')
-<body style="background: lightgray">
-	
-		<div class="card border-0 shadow rounded">
-			<div class="card-body">
-				<div style="text-align: right">
-					<a href="#" class="btn btn-primary btn-sm mb-3"><i class="fas fa-plus"></i> Tambah Data</a>
-				</div>
-							<table class="table table-bordered">
-								<thead>
-									<tr>
-										<th>Title</th>
-										<th>Slug</th>
-										<th>Experct</th>
-										<th>Body</th>
-									</tr>
-								</thead>
-								<tbody>
-									@forelse($posts as $post)
-									<tr>
-										<!-- <td>{{$loop->iteration}}</td> -->
-										<td>{{ $post->title }}</td>
-										<td>{{ $post->slug }}</td>
-			              <td>{!! $post->excerpt !!}</td>
-			              <td>{!! $post->body !!}</td>
-			                         
-									</tr>
-									   @empty
-			              <div class="alert alert-danger">
-			                    Data Blog belum Tersedia.
-			              </div>
-			              @endforelse
-											<!-- link menampilkan halaman -->
-								</tbody>
-							</table>
-					<div class="d-flex justify-content-end">
-						{{$posts->links()}}
-					</div>
-			</div>
-		</div>
-</body>
+	<h1>Dashboard</h1>
 @endsection
-				 
+         
--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -9,23 +9,148 @@
 	<link rel="stylesheet" type="text/css" href="{{asset('fontawesome/css/solid.css')}}">
 	<link rel="stylesheet" type="text/css" href="{{asset('style/bootstrap/dist/css/bootstrap.min.css')}}">
 	<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+	<link rel="stylesheet" type="text/css" href="{{asset('style/dashboard.css')}}">
 	<link rel="stylesheet" type="text/css" href="{{asset('style/style.css')}}">
 </head>
 <body>
-
 	
-	@include('components._navbar')
-	<section class="isi">
-		<div class="row mx-auto">
-			<div class="col-md-3 mb-3">
-				@include('components._sidebar')
-			</div>
-				<div class="col-md-9">
-					@yield('content')
-				</div>
-			</div>
-		</div>
-	</section>
+		<style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+  </head>
+  <body>
+    
+<header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+  <a class="navbar-brand fs-4 col-md-3 col-lg-2 me-0 px-3" href="/">REXXCODE</a>
+  <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+<!--   <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search"> -->
+  <div class="navbar-nav">
+    <div class="nav-item text-nowrap">
+    	<form action="/logout" method="POST">
+        	@csrf
+        	<button type="submit" class="btn btn-danger btn-md px-3 mx-3"><i class="fas fa-sign-out-alt"></i> Logout</button>
+		 </form>
+		
+    </div>
+  </div>
+</header>
+
+<div class="container-fluid">
+  <div class="row">
+    <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+      <div class="position-sticky pt-3">
+        <ul class="nav flex-column">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">
+              <span data-feather="home"></span>
+              Dashboard
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="file"></span>
+              Orders
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="shopping-cart"></span>
+              Products
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="users"></span>
+              Customers
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="bar-chart-2"></span>
+              Reports
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="layers"></span>
+              Integrations
+            </a>
+          </li>
+        </ul>
+
+        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+          <span>Saved reports</span>
+          <a class="link-secondary" href="#" aria-label="Add a new report">
+            <span data-feather="plus-circle"></span>
+          </a>
+        </h6>
+        <ul class="nav flex-column mb-2">
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="file-text"></span>
+              Current month
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="file-text"></span>
+              Last quarter
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="file-text"></span>
+              Social engagement
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="file-text"></span>
+              Year-end sale
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+      <!-- <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">Dashboard</h1>
+        <div class="btn-toolbar mb-2 mb-md-0">
+          <div class="btn-group me-2">
+            <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
+            <button type="button" class="btn btn-sm btn-outline-secondary">Export</button>
+          </div>
+          <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle">
+            <span data-feather="calendar"></span>
+            This week
+          </button>
+        </div>
+      </div> -->
+
+     <!--  <canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas> -->
+
+      @yield('content')
+     
+    </main>
+  </div>
+</div>
+
 	<!-- <div class="container mt-3">
 		
 		<h1>Hello World</h1>
@@ -34,6 +159,7 @@
 
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 	<script src="{{asset('style/bootstrap/dist/js/bootstrap.min.js')}}"></script>
+	<script src="{{asset('style/dashboard.js')}}"></script>
 	<script src="https://cdn.ckeditor.com/4.13.1/standard/ckeditor.js"></script>
 	<script>
 	    CKEDITOR.replace('content' );
